893b84674fff56064cc6b7e7127814b982747ee0
Zaid Pasha <zaidzpasha@gmail.com>
2019-11-14 11:26:01 -0800

    WorkingCarousel

--- frontend/components/browse/browse.jsx
+++ frontend/components/browse/browse.jsx
@@ -1,21 +1,33 @@
 import React from 'react';
 import GreetingContainer from '../greeting/greeting_container'
+import Slider from "react-slick";
 
 
 class BrowsePage extends React.Component {
-   
-  
+
+
 
     render(){
+       
+        var settings = {
+            infinite: true,
+            speed: 500,
+            slidesToScroll: 4,
+            slidesToShow: 6,
+            
+        };
         
         return(
+          
             <div className="mainerDiv">
-            
+                < link rel="stylesheet" type="text/css" charSet="UTF-8" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" />
+                <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" /> 
                 <GreetingContainer /> 
                 <div className="mainBrowseDiv">
                 <div className="mainvideo">
                 {/* <iframe className="Video"  src="https://www.youtube.com/embed/atxYe-nOa9w?controls=0&amp;start=42" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" width="560" height="760"></iframe> */}
-                        <video id="myVideo" src="./myhero.mp4" autoPlay></video>
+                        <video id="myVideo" src="./myhero.mp4" ></video>
+                        {/* aws src="https://onebucketman-seed.s3-us-west-1.amazonaws.com/My+Hero+Academia+-+Opening+2+_+Peace+Sign+(1080p_24fps_H264-128kbit_AAC).mp4" */}
 
                         {/* <video src="https://onebucketman.s3-us-west-1.amazonaws.com/My+Hero+Academia+-+Opening+2+_+Peace+Sign+(1080p_24fps_H264-128kbit_AAC).mp4" width="250px" height="250px"></video> */}
 
@@ -28,21 +40,55 @@ class BrowsePage extends React.Component {
                  </div>
                 </div>
             </div>
-            <div className="movieRow">
-
+             <div className="movieRow">
+                    <div className="mainSlider" >
+                    <div className="thumbnailHolder">
+                        <div className="header">
+                            <label htmlFor="">
+                                Title Header
+                            </label>
+                        </div> 
+                         <div>
+                          
+                            <Slider className="carcar"{...settings}>
+                            <div  >
+                            <img  src="./onepunch.jpg"   alt=""/>
+                            </div>
+                                <div >
+                                    <img class="item" src="./onepunch.jpg"  alt="" />
+                                </div>
+                                <div  >
+                                        <img class="item"src="./onepunch.jpg"  alt="" />
+                                </div>
+                                <div  >
+                                        <img class="item"src="./onepunch.jpg"  alt="" />
+                                </div>
+                                <div >
+                                        <img class="item"src="./onepunch.jpg"  alt="" />
+                                </div>
+                                <div >
+                                        <img class="item"src="./onepunch.jpg"  alt="" />
+                            </div>
+                                <div >
+                                        <img class="item"src="./onepunch.jpg"  alt="" />
+                            </div>
+                            <div>
+                                        <img class="item"src="./onepunch.jpg"  alt="" />
+                            </div>
+                            <div>
+                                        <img class="item"src="./onepunch.jpg"  alt="" />
+                            </div>
+                            <div>
+                                        <img class="item"src="./onepunch.jpg"  alt="" />
+                            </div>
+                        </Slider>
+                      
+                            </div>
                     
-                    <img src="./demonslayer.jpg" height="213.5" width="120" alt=""/>
+                            </div>
                    
-                    
-                        <img src="./demonslayer.jpg" height="213.5" width="120" alt="" />
-                    
-                        <img src="./demonslayer.jpg" height="213.5" width="120" alt="" />
-                   
-                    
-                        <img src="./demonslayer.jpg" height="213.5" width="120" alt="" />
-                   
-               
                  </div>
+            </div>
             </div>
         )
     }
